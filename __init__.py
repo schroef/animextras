987474990b84774a1fd15da6a028d77403b10b70
Rombout Versluijs <romboutv@gmail.com>
2023-09-25 15:22:46 -0400

    Test using new set_onion select method. Now user can set the onion object easier using enum menu. The user doesnt need to go out of pose mode and select the mesh object. Now this can bet set from the Onion Object enum menu. See issue #25

--- __init__.py
+++ __init__.py
@@ -1,36 +1,202 @@
+"""
+Updated
+- Panel layout > updated to match 2.8 styling
+- Added cleaner eye toggles for past and future
+- Icons to some buttons
+
+Added
+- Option to work with linked rigs > needs work InBetween as we need to target Parent rig
+- In Front option > show mesh in front of onion skinning
+- Shortcuts > for easier and faster workflow
+- Addon preferences so shortcuts can be customized
+- Panel feedback when nothings is selected or wrong object > updated slight. When onion obj in scene keep showing
+- Changing mode now auto-updates > a lot smoother > perhaps do that with all settings? It would mean lots of calculations running
+- Auto-update > updates onion object on opening file or when settings Mode, step change > Faster workflow
+- InBetweening can be toggle independed now, also can be turned of like other modes
+
+Fixed
+- Possibly old onion skinning when another file is openened
+- Linked rigs and local object/mesh also show onion skinning
+- InBetweening always shows, also when both future and past are disabled
+- Old print statement in registers
+- anmx_data type was not removed on deactiveing addon
+
+Todo
+- Updated on load_post
+- No active object with linked setup causes drawing issue
+
+Ideas
+- Auto update when working on posing > could be handy? > need feedback from real animators
+  > Simply add a bool and add update tool all props then use update_function to redraw
+- Added option to do multiple objects > this would need merge of objects, not sure will still work properly
+
+# 2023-08-23
+# Fixed
+# - issue with starting from linked rig
+"""
+
 ##################
-## Initiation
+#Initiation
 ##################
 
 bl_info = {
     "name": "AnimExtras",
-    "author": "Andrew Combs",
-    "version": (1, 1, 0),
+    "author": "Andrew Combs, Rombout Versluijs",
+    "version": (1, 1, 7),
     "blender": (2, 80, 0),
     "description": "True onion skinning",
-    "category": "Animation"
+    "category": "Animation",
+    "wiki_url": "https://github.com/iBrushC/animextras",
+	"tracker_url": "https://github.com/iBrushC/animextras/issues" 
 }
 
 import bpy
+import rna_keymap_ui
+from bpy.types import AddonPreferences
+
 from .ons.gui import *
-from .ons.ops import *
+from .ons import ops
+from .ons import registers
 
-# All classes that need to be initialized
-classes = [ANMX_gui, ANMX_data, ANMX_set_onion, ANMX_draw_meshes, ANMX_clear_onion, ANMX_update_onion]
 
-# Register function
+class ANMX_AddonPreferences(AddonPreferences):
+    """ Preference Settings Addon Panel"""
+    bl_idname = __name__
+    bl_label = "Addon Preferences"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def draw(self, context):
+        layout = self.layout
+        col = layout.column()
+
+        col.label(text = "Hotkeys:")
+        col.label(text = "Do NOT remove hotkeys, disable them instead!")
+
+        col.separator()
+        wm = bpy.context.window_manager
+        kc = wm.keyconfigs.user
+
+        col.separator()
+        km = kc.keymaps["3D View"]
+
+        kmi = registers.get_hotkey_entry_item(km, "anim_extras.update_onion","EXECUTE","tab")
+        if kmi:
+            col.context_pointer_set("keymap", km)
+            rna_keymap_ui.draw_kmi([], kc, km, kmi, col, 0)
+        else:
+            col.label(text = "Update Onion Object")
+            col.label(text = "restore hotkeys from interface tab")
+        col.separator()
+        
+        kmi = registers.get_hotkey_entry_item(km, "anim_extras.toggle_onion","EXECUTE","tab")
+        if kmi:
+            col.context_pointer_set("keymap", km)
+            rna_keymap_ui.draw_kmi([], kc, km, kmi, col, 0)
+        else:
+            col.label(text = "Toggle Draw Onion")
+            col.label(text = "restore hotkeys from interface tab")
+        col.separator()
+        
+        kmi = registers.get_hotkey_entry_item(km, "anim_extras.add_clear_onion","EXECUTE","tab")
+        if kmi:
+            col.context_pointer_set("keymap", km)
+            rna_keymap_ui.draw_kmi([], kc, km, kmi, col, 0)
+        else:
+            col.label(text = "Add / Clear Onion Object")
+            col.label(text = "restore hotkeys from interface tab")
+        col.separator()
+
+
+addon_keymaps = []
+classes = [ANMX_gui, ANMX_data, ANMX_set_onion, ANMX_draw_meshes, ANMX_clear_onion, ANMX_toggle_onion, ANMX_update_onion, ANMX_add_clear_onion, ANMX_AddonPreferences]
+global _delay
+_delay = 10
+
+@persistent
+def ANMX_clear_handler(dummy):
+    ops.clear_active(clrObj=False, clrRig=False)
+    anmx = bpy.context.scene.anmx_data
+    if ANMX_draw_meshes.handler is not None:
+        ANMX_draw_meshes.unregister_handlers(None, bpy.context)
+
+    if anmx.onion_object != "" and anmx.auto_update and anmx.toggle:
+        bpy.ops.anim_extras.update_onion()
+        bpy.ops.anim_extras.draw_meshes('INVOKE_DEFAULT')
+    # else:
+    #     anmx.toggle = False
+
+    #     ANMX_draw_meshes.finish(None,bpy.context)
+    # ANMX_draw_meshes.unregister_handlers(ANMX_draw_meshes.handler, bpy.context)
+    # bpy.ops.anim_extras.draw_meshes('INVOKE_DEFAULT')
+
+# Very buggy, shortcut is more convenient plus less demanding of the system
+"""
+def execute_queued_functions():
+    anmx = bpy.context.scene.anmx_data
+    obj = bpy.context.active_object
+    print("#### _delay {} ####".format(_delay))
+    if obj.mode == 'POSE' and anmx.auto_key:
+        print("#### _delay {} ####".format(float(getattr(anmx,"key_intval"))))
+        bpy.ops.anim_extras.update_onion()
+        return float(getattr(anmx,"key_intval"))
+    return _delay
+
+@persistent
+def ANMX_update_pose(dummy):
+    obj = bpy.context.active_object
+    anmx = bpy.context.scene.anmx_data
+    global _delay 
+    _delay = 10  
+    # _timer = False
+    # bpy.app.timers.register(execute_queued_functions)
+    # timer = bpy.context.window_manager.event_timer_add(1, window=bpy.context.window)
+    if ANMX_draw_meshes.handler is not None and bpy.context.selected_objects != []:
+        if anmx.onion_object != "" and anmx.auto_update and anmx.toggle:
+            onionObj = anmx.onion_object
+            onObj = [obj for obj in bpy.context.selected_objects if ('{0}'.format(onionObj)) in obj.name]
+            if onObj !=[]:
+                if onObj[0].name == bpy.context.active_object.name:
+                    _delay = 1
+                    print("#### _delay {} ####".format(_delay))
+                    bpy.app.timers.register(execute_queued_functions)
+            if obj.mode == 'POSE':
+                if _delay == 1:
+                    _delay = 10    
+                    print("#### _delay {} ####".format(_delay))
+  """             
+
 def register():
     for c in classes:
         bpy.utils.register_class(c)
     
-	# External pointer property definitions
     bpy.types.Scene.anmx_data = bpy.props.PointerProperty(type=ANMX_data)
+    bpy.app.handlers.load_post.append(ANMX_clear_handler)
     
-# Unregister function
+    wm = bpy.context.window_manager
+    kc = wm.keyconfigs.addon
+    km = kc.keymaps.new(name="3D View", space_type="VIEW_3D")
+
+    kmi = km.keymap_items.new("anim_extras.update_onion", "R", "PRESS", alt = True, shift = True)
+    addon_keymaps.append((km, kmi))
+    
+    kmi = km.keymap_items.new("anim_extras.toggle_onion", "T", "PRESS", alt = True, shift = True)
+    addon_keymaps.append((km, kmi))
+    
+    kmi = km.keymap_items.new("anim_extras.add_clear_onion", "C", "PRESS", alt = True, shift = True)
+    addon_keymaps.append((km, kmi))
+
+
 def unregister():
+    bpy.app.handlers.load_post.remove(ANMX_clear_handler)
+    
     for c in classes:
         bpy.utils.unregister_class(c)
-    
+
+    for km, kmi in addon_keymaps:
+        km.keymap_items.remove(kmi)
+    addon_keymaps.clear()
+
+    del bpy.types.Scene.anmx_data
 
 if __name__ == "__main__":
     register()
--- ons/gui.py
+++ ons/gui.py
@@ -1,12 +1,13 @@
 #######################
 ## Onion Skinning GUI
 #######################
+
 import bpy
 from .ops import *
 
 
 class ANMX_gui(bpy.types.Panel):
-    """Panel for all onion skinning Operations"""
+    """Panel for all Onion Skinning Operations"""
     bl_idname = 'VIEW3D_PT_animextras_panel'
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
@@ -14,86 +15,122 @@ class ANMX_gui(bpy.types.Panel):
     bl_label = 'Onion Skinning'
 
     
-    # All functions for drawing the panel go in here
-    # TODO:
-    # -- Seperate different areas (which control toggleable areas) into seperate functions
-    
     def draw(self, context):
-        # All predefined variables
         layout = self.layout
-        access = context.scene.anmx_data
-        obj = context.object
+        scn = context.scene
+        anmx = context.scene.anmx_data
+        obj = context.active_object
+
+        # Makes UI split like 2.8 no split factor 0.3 needed
+        layout.use_property_split = True
+        layout.use_property_decorate = False
         
+        if obj:
+            # Show info to user when ietm is not linked or animated
+            if not (hasattr(obj.animation_data,"action")) and obj.instance_collection == None:
+                layout.label(text="Selected object has no animation", icon='INFO')
+            # if context.selected_objects == [] and anmx.onion_object == "":
+            #     layout.label(text="Nothing selected", icon='INFO')
         # Makes sure the user can't do any operations when the onion object doesn't exist
-        if access.onion_object not in bpy.data.objects:
+        if anmx.onion_object not in bpy.data.objects:
             layout.operator("anim_extras.set_onion")
             return
+            # return
+        # if not ((obj.type == 'MESH') and hasattr(obj.animation_data,"action") or (obj.type=='EMPTY')):
+        #     layout.label(text="Update needs active object", icon='INFO')
+            # return    
         else:
             row = layout.row(align=True)
-            row.operator("anim_extras.update_onion", text="Update")
-            row.operator("anim_extras.clear_onion", text="Clear Selected")
+            # sub = row.row(align=True)
+            row.prop(anmx, "auto_update", text="", icon="FILE_REFRESH", toggle=True)
+            sub = row.row(align=True)
+            sub.enabled = anmx.auto_update == False
+            sub.operator("anim_extras.update_onion", text="Update")
+            # row = row
+            row.operator("anim_extras.clear_onion", text="Clear")
+            layout.separator(factor=0.2)
         
-        # Top label for the onion object. This may be replaced with a list if support for multiple onion objects is added
+        
         col = layout.column()
-        col.label(text="Current Onion Object: {}".format(access.onion_object))
+        # col.prop(anmx,"onion_object", text="Current", emboss=False, icon='OUTLINER_OB_MESH') #text="{}".format(anmx.onion_object), 
+        # Issue when using emboss=false > the whole column receives this. Feels like a API bug
+        if not obj.type == 'ARMATURE':
+            col.label(text="Please select RIG", icon='INFO')
+            # return
+        else:
+            col.prop(anmx,"set_onion_object", text="Onion Object", icon='OUTLINER_OB_MESH') #text="{}".format(anmx.onion_object), 
         
-        # Dropdown menu for the onion mode
-        col.prop(access, "onion_mode")
-        col.separator(factor=1)
+        col = layout.column()
+        col.prop(anmx, "onion_mode", text="Method")
         
-        # Area for changing the amount of onion skins
-        row = layout.row()
-        row = row.split(factor=0.3)
-        row.label(text="Amount:")
-        row.prop(access, "skin_count", text="")
-        
-        # Special exception for if the mode is Per-Frame-Stepped
-        # TODO:
-        # -- Make this better. This isn't a futureproof method, and should probably be expandable
-        if access.onion_mode == "PFS":
+        modes = {"PFS", "INB"}
+        # if not anmx.onion_mode in modes: #
+        if anmx.onion_mode != "PFS":
             row = layout.row()
-            row = row.split(factor=0.3)
-            row.label(text="Step:")
-            row.prop(access, "skin_step", text="")
+            row.prop(anmx, "skin_count", text="Amount")
+
+        if anmx.onion_mode == "PFS":
+            col = layout.column(align=True)
+            col.prop(anmx, "skin_count", text="Amount")
+            col.prop(anmx, "skin_step", text="Step")
         
-        # Boxes for selecting the past and future onion skinning options
-        
-        text = "Past"  # A text variable is used so that the box labels can be changed (different modes require different settings)
-        if access.onion_mode == "INB":
+        text = "Past"
+        if anmx.onion_mode == "INB":
             text = "Inbetween Color"
-        # Past box
+        
         row = layout.row(align=True)
         box = row.box()
         col = box.column(align=True)
-        col.label(text=text)
-        col.prop(access, "past_color", text="")
-        col.prop(access, "past_opacity_start", text="Start Opacity", slider=True)
-        col.prop(access, "past_opacity_end", text="End Opacity", slider=True)
-        col.prop(access, "past_enabled")
+        past = col.row(align=True)
+        icoPast = 'HIDE_OFF' if anmx.past_enabled else 'HIDE_ON'
+        past.row().prop(anmx, "past_enabled", text='', icon=icoPast, emboss=False)
+        past.row().label(text=text)
+        col.prop(anmx, "past_color", text="")
+        col.prop(anmx, "past_opacity_start", text="Start Opacity", slider=True)
+        col.prop(anmx, "past_opacity_end", text="End Opacity", slider=True)        
         
-        
-        text = "Future"  # See text = "Past"
-        if access.onion_mode == "INB":
+        text = "Future"
+
+        if anmx.onion_mode == "INB":
             text = "Direct Keying Color"
-        # Future box
+        
         box = row.box()
         col = box.column(align=True)
-        col.label(text=text)
-        col.prop(access, "future_color", text="")
-        col.prop(access, "future_opacity_start", text="Start Opacity", slider=True)
-        col.prop(access, "future_opacity_end", text="End Opacity", slider=True)
-        col.prop(access, "future_enabled")
+        fut = col.row(align=True)
+        icoFut = 'HIDE_OFF' if anmx.future_enabled else 'HIDE_ON'
+        fut.prop(anmx, "future_enabled", text='', icon=icoFut, emboss=False)
+        fut.label(text=text)
+        col.prop(anmx, "future_color", text="")
+        col.prop(anmx, "future_opacity_start", text="Start Opacity", slider=True)
+        col.prop(anmx, "future_opacity_end", text="End Opacity", slider=True)
         
-        layout.separator(factor=1)
+        layout.use_property_split = True
+        layout.use_property_decorate = False  # No animation.
+        layout.separator(factor=0.2)
+        if (bpy.app.version[1] < 92):
+            col = layout.column(align=True)
+            col.label(text="Options")
+        else:
+            col = layout.column(heading="Options", align=True)
+        col.prop(anmx, "use_xray")
+        col.prop(anmx, "use_flat")
+        col.prop(anmx, "in_front")
+
+        # col.use_property_split = True
+        # col.use_property_decorate = False
+
+        # if scn.tool_settings.use_keyframe_insert_auto:
+        #     key = col.column(align=True)
+        #     # key.enabled = (scn.tool_settings.use_keyframe_insert_auto and (obj.mode == 'POSE')) == True
+        #     key.prop(anmx, "auto_key")
+        #     # key = layout.column(heading="Interval", align=True)
+        #     key.prop(anmx, "key_intval", text="Interval", icon="SORTTIME")
+ 
+        layout.use_property_split = False
+        layout.separator(factor=0.2)
         
-        # Settings for xray and flat shading
-        layout.prop(access, "use_xray")
-        layout.prop(access, "use_flat")
-        
-        layout.separator(factor=1)
-        
-        # Drawing toggle button., variable is used to display status
         text = "Draw"
-        if access.toggle:
+        if anmx.toggle:
             text = "Stop Drawing"
-        layout.prop(access, "toggle", text=text, toggle=True)
+        icoOni = 'ONIONSKIN_OFF' if anmx.toggle else 'ONIONSKIN_ON'
+        layout.prop(anmx, "toggle", text=text, toggle=True, icon=icoOni)
